901bae53ffa1108f5bc42ef5216497c2ece3baf6
Derry Leng <derry.leng@gmail.com>
2023-08-27 19:08:04 +0000

    Sync LeetCode submission - Find Pivot Index (cpp)

---
+++ problems/find_pivot_index/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int lSum = 0;
+        int rSum = accumulate(nums.begin(), nums.end(), 0);
+        for (int i = 0; i < nums.size(); i++) {
+            rSum -= nums[i];
+            if (lSum == rSum) {
+                return i;
+            }
+            lSum += nums[i];
+        }
+        return -1;
+    }
+};
