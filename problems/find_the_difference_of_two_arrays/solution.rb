7cdc6fe49454bcc17584bd804047bcd960a4a251
Derry Leng <derry.leng@gmail.com>
2023-09-24 13:54:42 +0000

    Sync LeetCode submission - Find the Difference of Two Arrays (ruby)

--- problems/find_the_difference_of_two_arrays/solution.rb
+++ problems/find_the_difference_of_two_arrays/solution.rb
@@ -1,8 +1,12 @@
+# Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+# answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+# answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+# Note that the integers in the lists may be returned in any order.
+
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Integer[][]}
 def find_difference(nums1, nums2)
-    n1 = nums1.uniq
-    n2 = nums2.uniq
-    return [n1 - n2, n2 - n1]
+    return [(nums1 - nums2).uniq, (nums2 - nums1).uniq]
 end
