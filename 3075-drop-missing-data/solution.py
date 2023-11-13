ea8fec8ab680dbd396da6d71541751b00bb9b9aa
Derry Leng <derry.leng@gmail.com>
2023-11-13 12:56:50 +0000

    Sync LeetCode submission Runtime - 611 ms (8.42%), Memory - 60.8 MB (43.77%)

---
+++ 3075-drop-missing-data/solution.py
@@ -0,0 +1,20 @@
+# DataFrame students
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | student_id  | int    |
+# | name        | object |
+# | age         | int    |
+# +-------------+--------+
+
+# There are some rows having missing values in the name column.
+
+# Write a solution to remove the rows with missing values.
+
+# The result format is in the following example.
+
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset=["name"])
+
